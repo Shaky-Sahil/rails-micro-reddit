8ab5fbfc06311ac5ac8628a9ce561881feedca53
shaky-sahil <sahilpk81@gmail.com>
2021-12-23 17:31:53 +0530

    fixed typo in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,3 @@
 class User < ApplicationRecord
-    validates :name, presence: true, uniqueness: true, length: { in: 3..20}
+    validates :username, presence: true, uniqueness: true, length: { in: 3..20}
 end
