7bb95f15c810ab3b15239abf20f4b3e656064df1
shaky-sahil <sahilpk81@gmail.com>
2021-12-24 11:13:22 +0530

    added association b/w user and posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ApplicationRecord
     validates :title, presence: true,length: { maximum: 50 }
     validates :user_id, uniqueness: true
     validates :body, presence: true
+    belongs_to :user
 end
