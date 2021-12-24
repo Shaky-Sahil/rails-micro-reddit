0bff6a4043c694306de2657d1bbe7207277607d9
shaky-sahil <sahilpk81@gmail.com>
2021-12-24 11:23:05 +0530

    fixed post validation to allow multiple posts for a user

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
     validates :title, presence: true,length: { maximum: 50 }
-    validates :user_id, uniqueness: true
+    validates :user_id, presence: true
     validates :body, presence: true
     belongs_to :user
 end
